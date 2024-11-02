7dbf6fc505b806d340cab2b75ce2ba86d745b733
FrankApiyo <franklineapiyo@gmail.com>
2024-11-02 08:23:46 +0300

    Initial commit: mix phx.new littlechat --live

---
+++ lib/littlechat_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule LittlechatWeb.PageController do
+  use LittlechatWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
