2624b38664a396ced981aff5d0c79eea76450a4f
FrankApiyo <franklineapiyo@gmail.com>
2024-11-04 08:34:46 +0300

    Add a rooms table, organizer context and migrations

---
+++ lib/littlechat/organizer/room.ex
@@ -0,0 +1,33 @@
+defmodule Littlechat.Organizer.Room do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "rooms" do
+    field :title, :string
+    field :slug, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @fields [:title, :slug]
+
+  @doc false
+  def changeset(room, attrs) do
+    room
+    |> cast(attrs, @fields)
+    |> validate_required(@fields)
+    |> format_slug()
+    |> unique_constraint(:slug)
+  end
+
+  defp format_slug(%Ecto.Changeset{changes: %{slug: _}} = changeset) do
+    changeset
+    |> update_change(:slug, fn slug ->
+      slug
+      |> String.downcase()
+      |> String.replace(" ", "-")
+    end)
+  end
+
+  defp format_slug(changeset), do: changeset
+end
