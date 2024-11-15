457105d65bdd680a429931d1d539fab997c40bce
FrankApiyo <franklineapiyo@gmail.com>
2024-11-15 08:46:16 +0300

    Add client side WebRTC flow

--- assets/js/app.js
+++ assets/js/app.js
@@ -68,6 +68,78 @@ Hooks.InitUser = {
   }
 }
 
+// lv        - Our LiveView hook's `this` object
+// fromUser  - The user to create the peer connection with
+// offer     - Stores an SDP offer if it was passed to the function
+function createPeerConnection(lv, fromUser, offer) {
+  let newPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      { urls: "stun:stun.l.google.com:19302" },
+      { urls: "stun:stun.l.google.com:5349" },
+      { urls: "stun:stun1.l.google.com:3478" },
+      { urls: "stun:stun1.l.google.com:5349" },
+      { urls: "stun:stun2.l.google.com:19302" },
+      { urls: "stun:stun2.l.google.com:5349" },
+      { urls: "stun:stun3.l.google.com:3478" },
+      { urls: "stun:stun3.l.google.com:5349" },
+      { urls: "stun:stun4.l.google.com:19302" },
+      { urls: "stun:stun4.l.google.com:5349" }
+    ]
+  }
+  )
+
+  // Add this peer connection to our user object
+  users[fromUser].peerConnection = newPeerConnection
+
+  // Add each local track to the RTCPeerConnection
+  localStream.getTracks().forEach(track => newPeerConnection.addTrack(track, localStream))
+
+  // If creating an answer rather than the initial offer.
+  if (offer !== undefined) {
+    newPeerConnection.setRemoteDescription({ type: "offer", sdp: offer })
+    newPeerConnection.createAnswer()
+      .then((answer) => {
+        newPeerConnection.setLocalDescription(answer)
+        console.log("Sending this answer to the requester: ", answer)
+        lv.pushEvent("new_answer", { toUser: fromUser, description: answer })
+      })
+      .catch((err) => console.log(err))
+  }
+
+  newPeerConnection.onicecandidate = async ({ candidate }) => {
+    // fromUser is the new value of toUser because we're sending this data back
+    // to the sender
+    lv.PushEvent("new_ice_candidate", { toUser: fromUser, candidate })
+  }
+
+  // Don't add the `onnegotiationneeded` callback when creating and answer due to
+  // a bug in Chrome's implementation of WebRTC
+  if (offer == undefined) {
+    newPeerConnection.onnegotiationneeded = async () => {
+      try {
+        newPeerConnection.createOffer()
+          .then((offer) => {
+            newPeerConnection.setLocalDescription(offer)
+            console.log("Sending this offer to the requester: ", offer)
+            lv.pushEvent("new_sdp_offer", { toUser: fromUser, description: offer })
+          })
+          .catch((err) => console.log(err))
+      }
+      catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
+  // When the data is ready to flow, add it to the correct video
+  newPeerConnection.ontrack = async (event) => {
+    console.log("Track received: ", event)
+    document.getElementById(`video-remote-${fromUser}`).srcObject = event.streams[0]
+  }
+
+  return newPeerConnection
+}
+
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
   hooks: Hooks,
